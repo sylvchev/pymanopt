07a211c0b2c29f41fc93a4f2b1744b4e3396ba23
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2019-03-25 13:49:30 +0100

    example of checkgradient

---
+++ examples/dominant_eigenvector_gradient.py
@@ -0,0 +1,30 @@
+import autograd.numpy as np
+from pymanopt.manifolds import Sphere
+from pymanopt import Problem
+from pymanopt.tools.utils import checkgradient
+from pymanopt.solvers import TrustRegions
+
+# Generate random problem data.
+n = 1000
+A = np.random.randn(n)
+A = .5 * (A + A.T)
+     
+# Create the problem structure.
+manifold = Sphere(n)
+     
+# Define the problem cost function and its Euclidean gradient.
+def cost(x):
+    return -x.T @ (A * x)
+
+def egrad(x):
+    # notice the 'e' in 'egrad' for Euclidean
+    return -2 * A * x
+
+problem = Problem(manifold=manifold, cost=cost, egrad=egrad)
+
+# Numerically check gradient consistency (optional).
+checkgradient(problem)
+     
+# Solve
+solver = TrustRegions()
+wopt = solver.solve(problem)
